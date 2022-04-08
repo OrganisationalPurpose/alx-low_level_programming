1be1d1fa43ec645a89df1ea3e0313b0de68ce308
IanoNjuguna <ichege438@gmail.com>
2022-04-08 22:46:58 +0000

    Correcting syntax 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  * main -  the entry point the starting point
- *   * Return: Always 0 (Success)
- *    */
+ * * main - main block
+ * * Return: 0
+ * */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-					return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
