6d3d430906ede07e2a6ad9abb764beb26126b699
IanoNjuguna <ichege438@gmail.com>
2022-05-19 17:01:39 +0300

    Final Documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,10 @@
+/*
+A C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+Using the function 'puts'
+Not allowed to use printf
+Program should end with the value 0
+*/
+
 #include <stdio.h>
 
 /**
