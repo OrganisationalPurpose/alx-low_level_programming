d371fb97e5aaa5f5039001ebd50a7f4c589601d5
IanoNjuguna <ichege438@gmail.com>
2022-05-19 17:08:51 +0300

    Final Documentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,10 @@
+/**
+A C program that prints exactly 'with proper grammar, but the outcome is a piece of art,' followed by a new line.
+Use the function printf
+Not allowed to use the function puts
+Program should return 0
+Program should compile without warning when using the -Wall gcc option
+*/
 #include <stdio.h>
 /**
  *main - main block
