0d4673092ce5335b79887f562d8e37944e3dda7c
IanoNjuguna <ichege438@gmail.com>
2022-10-05 12:26:17 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,6 @@
  * puts("\"Programming is like building a multilingual puzzle)
  * Return: Always 0(Success)
  */
-
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
