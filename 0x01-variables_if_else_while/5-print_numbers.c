67b940ca55851d623c4c2b6a9158c709f99f0c41
IanoNjuguna <ichege438@gmail.com>
2022-10-17 05:12:07 +0300

    Betty Compliance

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,3 @@
-/**
- *A program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
- *All my code should is in the main function
- * 
- */
-
 #include <stdio.h>
 
 /**
