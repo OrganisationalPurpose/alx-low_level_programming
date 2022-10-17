45ebb48564b00698de2d7dd93389fb145f52f1a2
IanoNjuguna <ichege438@gmail.com>
2022-10-17 04:28:05 +0300

    Betty compliant

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,3 @@
-/**
- * A program will assign a random number to the variable (n) each time it is executed whether the number stored in the variable (n) is positive or negative.
- * The variable n stores a different value every time this program is run
- * The output of the program should be:
-     The number, followed by:
-     -If the number is greater than 0: is positive
-     -If the number is 0: is zero
-     -If the number is less than 0: is negative
-     -A new line
- */
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -26,12 +15,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n > 1)
-printf("%d is positive\n", n);
+{
+	printf("%d is positive\n", n);
+}
 else if (n < 0)
-printf("%d is negative\n", n);
+{
+	printf("%d is negative\n", n);
+}
 else
-
-printf("%d is zero\n", n);
-
+{
+	printf("%d is zero\n", n);
+}
 return (0);
 }
