9d1fb2bcc37cfde7e14256486a5e9e4c52a066bf
IanoNjuguna <ichege438@gmail.com>
2022-11-17 13:05:57 +0300

    100-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-int digit1, digit2, digit3;
+int integer_1, integer_2, integer_3;
 
-for (digit1 = 0; digit1 < 8; digit1++)
+for (integer_1 = 0; integer_1 < 8; integer_1++)
 {
-for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+for (integer_2 = integer_1 + 1; integer_2 < 9; integer_2++)
 {
-for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+for (integer_3 = integer_2 + 1; integer_3 < 10; integer_3++)
 {
-putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
-putchar((digit3 % 10) + '0');
+putchar(integer_1 + '0');
+putchar(integer_2 + '0');
+putchar(integer_3 + '0');
 
-if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+if (integer_1 == 7 && integer_2 == 8 && integer_3 == 9)
 continue;
 
 putchar(',');
