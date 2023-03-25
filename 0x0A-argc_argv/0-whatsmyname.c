4c788c6f8eb4dd168ce458d3bc16abe1d2b8fb79
IanoNjuguna <inc3099@gmail.com>
2023-03-25 19:57:55 +0300

    prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main -  writes its name
- * @argc: arguement count
- * @argv: array of pointers to arguement strings
- * Return: 0
- **/
-int main(int argc, char *argv[])
+ * main - prints its name, followed by a new line
+ *
+ * @argc: number of arguments passed
+ * @argv: array of arguments passed
+ *
+ * Return: EXIT_SUCCESS
+ * aUTHOR: IanoNjuguna
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
 {
-	argc = argc;
 	printf("%s\n", *argv);
-	return (0);
+
+	exit(EXIT_SUCCESS);
 }
+
