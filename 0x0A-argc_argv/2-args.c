8c6548b337dc1e52ff8b49973f006ede25e8f016
IanoNjuguna <inc3099@gmail.com>
2023-03-26 13:38:00 +0300

    prints all arguments it receives, followed by a newline

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - a program that prints all arguments it receives.
- * @argc: arguement count
- * @argv: array of pointers to arguement strings
- * Return: 0
- **/
-int main(int argc, char *argv[])
+ * main - prints all arguments it receives
+ *
+ * @argc: number of arguments passed
+ * @argv: array of arguments passed
+ *
+ * Return: EXIT_SUCCESS
+ */
+
+int main(int argc, char **argv)
 {
 	while (argc--)
-		printf("%s\n", *argv++);
-	return (0);
+	{
+		printf("%s\n", *argv);
+		argv++;
+	}
+	exit(EXIT_SUCCESS);
 }
+
