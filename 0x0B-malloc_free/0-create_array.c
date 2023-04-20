4946508dbd8539a46ffc2847b51f0b121c0ed05b
Musashi <inc3099@gmail.com>
2023-04-20 22:18:44 +0300

    Create main.h file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars,
+ *				  and initializes it with a specific char
+ *
+ * Return: Size of memory allocated
+ * Author: IanoNjuguna
+ */
+char *create_array(unsigned int size, char c)
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+char *create_array(unsigned int size, char c);
+
+#endif
