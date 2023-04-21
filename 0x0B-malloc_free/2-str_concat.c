e671234bebc8b0a9e27d0fc02c40b773a88b3385
Musashi <inc3099@gmail.com>
2023-04-21 15:12:43 +0300

    Betty Coding Style: remove Block comments to fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,6 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int size;
 	char *concat_str;
-/*	char *arr;*/
 
 	if (s1 == NULL)
 		s1 = "";
@@ -21,7 +20,6 @@ char *str_concat(char *s1, char *s2)
 
 	size = strlen(s1) + strlen(s2) + 1;
 	concat_str = (char *) malloc(sizeof(char) * size);
-/*	arr = (char *) malloc(sizeof(char) * size);*/
 
 
 	if (size == 0 || concat_str == NULL)
@@ -29,16 +27,6 @@ char *str_concat(char *s1, char *s2)
 
 	strcpy(concat_str, s1);
 	strcat(concat_str, s2);
-
-/*	if (arr == NULL)
-	{
-		free(concat_str);
-		return (NULL);
-	}
-
-	strcpy(arr, concat_str);
-
-	free(concat_str);*/
 
 	return (concat_str);
 }
