8db050a0ca8561405ee93eb6cad42f55b8e36477
IanoNjuguna <inc3099@gmail.com>
2023-06-26 22:37:51 +0300

    100-times_table.c: format the spacing to meet the specs

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,7 +22,7 @@ void print_times_table(int n)
 	{
 		for (count2 = 0; count2 <= n; count2++)
 			{
-				printf("%d", count1 * count2);
+				printf("%2d", count1 * count2);
 
 				if (count2 == n)
 				{
